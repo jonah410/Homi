f1af099677a4dc61e745e3da6a4a4ad9701b8db1
Jonah Black <jonahblack@Jonahs-MacBook-Pro-3.local>
2024-05-30 10:31:42 -0500

    bound port to 0.0.0.0

--- server/app.js
+++ server/app.js
@@ -41,8 +41,8 @@ app.post('/api/blitz/get-embeddings', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
