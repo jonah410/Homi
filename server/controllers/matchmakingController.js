62bfe340514e50d6c7f3247da7647e1029a2ab21
Jonah Black <jonahblack@Jonahs-MBP-3.attlocal.net>
2024-05-30 16:23:49 -0500

    added updateDoc to matchmakingController

--- server/controllers/matchmakingController.js
+++ server/controllers/matchmakingController.js
@@ -1,4 +1,5 @@
 const { matchUsers } = require('../services/matchmakingService');
+const { doc, updateDoc } = require('firebase/firestore');
 
 const startMatchmaking = async (req, res) => {
   const { userId, location } = req.body;
@@ -9,17 +10,12 @@ const startMatchmaking = async (req, res) => {
     } else {
       // Even if no suitable matches found, still set "looking" to true
       await updateDoc(doc(db, 'users', userId), { looking: true });
-      res.status(200).json({ success: false, message: "We haven't found any suitable matches for you. But we're looking!" });
+      res.status(200).json({ success: false, message: "We haven't found any suitable matches for you yet. But we're looking!" });
     }
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
-
-module.exports = {
-  startMatchmaking,
-};
-
 
 module.exports = {
   startMatchmaking,
